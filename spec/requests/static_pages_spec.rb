3cfc111f39532876126430e08bcee3f538effc0a
Oleksandr Adamchuk <oleks.adamchuk@gmail.com>
2017-11-23 22:13:10 +0200

    Add root method to routes.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,11 +10,16 @@ RSpec.describe "StaticPages", type: :request do
 	    expect(page).to have_content('Sample App')
 		end
 
-		it "should have the right title" do
-		  visit '/static_pages/home'
-		  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-		end
+		it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
 
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+  
 	end
 
 	describe "Help page" do
@@ -43,7 +48,18 @@ RSpec.describe "StaticPages", type: :request do
     end
   end
 
+  describe "Contact page" do
 
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 
 
 
